eef696a695c5d3258f1f4735caa99c0dc5b7b1f2
Aakash R <aakashr2308@gmail.com>
2025-04-01 16:55:18 +0530

    QF

--- Main/src/firebase/firebase.js
+++ Main/src/firebase/firebase.js
@@ -3,15 +3,7 @@ import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
 import { getFirestore, collection, addDoc, onSnapshot, query, orderBy, doc, updateDoc, serverTimestamp } from 'firebase/firestore';
 import { getStorage, ref, uploadBytes, getDownloadURL } from 'firebase/storage';
 
-const firebaseConfig = {
-  apiKey: "AIzaSyD3vr5BMDA62_5Uq9V-fZKmJFNEUs2gjqE",
-  authDomain: "test-project-12f2a.firebaseapp.com",
-  projectId: "test-project-12f2a",
-  storageBucket: "test-project-12f2a.firebasestorage.app",
-  messagingSenderId: "77990698503",
-  appId: "1:77990698503:web:eccef63422a18c602794a2",
-  measurementId: "G-BGC05V1HSC"
-};
+const firebaseConfig = {};
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
